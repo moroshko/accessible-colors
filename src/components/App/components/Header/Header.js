0686d61b59ff5a1c85343c9cf2211caae3c44c7f
Misha Moroshko <michael.moroshko@gmail.com>
2015-08-18 17:05:24 +1000

    Add GitHub star button

--- src/components/App/components/Header/Header.js
+++ src/components/App/components/Header/Header.js
@@ -13,6 +13,18 @@ export default class Header extends Component {
           <p className={styles.subHeader}>
             Make any color combination accessible
           </p>
+          <p>
+            <a className="github-button"
+               href="https://github.com/moroshko/accessible-colors"
+               data-icon="octicon-star"
+               data-style="mega"
+               data-count-href="/moroshko/accessible-colors/stargazers"
+               data-count-api="/repos/moroshko/accessible-colors#stargazers_count"
+               data-count-aria-label="# stargazers on GitHub"
+               aria-label="Star moroshko/accessible-colors on GitHub">
+              Star
+            </a>
+          </p>
         </div>
       </div>
     );
