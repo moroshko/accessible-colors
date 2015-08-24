65f74819c4ca3476b104b04a4b9078ab231ce14e
Misha Moroshko <michael.moroshko@gmail.com>
2015-08-24 23:13:40 +1000

    Fix deploy-to-gh-pages script

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -7,7 +7,7 @@ git pull origin gh-pages
 git merge master --no-edit
 npm run build
 cp -r dist/* .
-git add index.html app.css icons.css index.js favicon.png fonts/AccessibleColor.eot AccessibleColor.svg AccessibleColor.ttf AccessibleColor.woff
+git add index.html app.css icons.css index.js favicon.png fonts/AccessibleColor.eot fonts/AccessibleColor.svg fonts/AccessibleColor.ttf fonts/ccessibleColor.woff
 git commit -m 'Update gh-pages files'
 git push origin gh-pages
 git checkout master
